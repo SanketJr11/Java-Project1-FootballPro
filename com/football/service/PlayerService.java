e3b79d06069ec8755d835d8d99c055e4a1221a8e
SanketJr11 <sanket643@gmail.com>
2025-11-18 12:10:31 +0000

    Add service layer: PlayerService and PlayerManager

---
+++ com/football/service/PlayerService.java
@@ -0,0 +1,33 @@
+package com.football.service;
+
+import com.football.model.Player;
+import java.util.*;
+import java.util.function.Predicate;
+
+/**
+ * Demonstrates:
+ * - interfaces
+ * - private, default, and static methods
+ * - use of generic List (Java Core API)
+ */
+public interface PlayerService {
+    void addPlayer(Player player);
+    void listPlayers();
+    Optional<Player> findPlayer(String name);
+    List<Player> filterPlayers(java.util.function.Predicate<Player> condition); // Lambda support
+
+    // Default method (Java 8+)
+    default void welcome() {
+        System.out.println("👋 Welcome to Football Manager!");
+    }
+
+    // Static method
+    static void separator() {
+        System.out.println("--------------------------");
+    }
+
+    // Private method (Java 9+)
+    private void log(String msg) {
+        System.out.println("[LOG] " + msg);
+    }
+}
